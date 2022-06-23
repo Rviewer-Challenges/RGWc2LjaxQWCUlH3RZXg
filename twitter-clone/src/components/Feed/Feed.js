03e91f7a162ad9626d434e9460dbb3bd2df596de
Samuel Taboas <droidsam@gmail.com>
2022-06-23 07:52:28 +0200

    add simple tweetbox to feed

--- twitter-clone/src/components/Feed/Feed.js
+++ twitter-clone/src/components/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TweetBox from "../TweetBox/TweetBox";
 import "./Feed.css";
 
 function Feed(props) {
@@ -7,8 +8,7 @@ function Feed(props) {
       <div className="header">
         <h2>Home</h2>
       </div>
-
-      {/* TweetBox */}
+      <TweetBox/>
       {/* Post */}
       {/* Post */}
       {/* Post */}
---
+++ twitter-clone/src/components/TweetBox/TweetBox.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TweetBox.css";
+import {Avatar, Button} from '@mui/material/';
+
+function TweetBox() {
+  return <div className="tweet-box">
+    <form>
+        <div className="input-box">
+        <Avatar src="https://avatars.githubusercontent.com/u/36738579?v=4"/>
+        <input type="text" placeholder="What's happening"></input>
+        </div>
+        <Button className="tweet-button">Tweet</Button>
+    </form>
+  </div>;
+}
+
+export default TweetBox;
