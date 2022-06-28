580b8c06224d8af09b06afa6b50590c5877fb85a
Samuel Taboas <droidsam@gmail.com>
2022-06-28 07:37:22 +0200

    add widgets styles

--- twitter-clone/src/components/Widgets/Widgets.js
+++ twitter-clone/src/components/Widgets/Widgets.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./Widgets.css";
+import SearchIcon from "@mui/icons-material/Search";
 function Widgets() {
-  return <div className="widgets">Widgets</div>;
+  return (
+    <div className="widgets">
+      <div className="widgets-input">
+        <SearchIcon className="search-icon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widget-container">
+        <h2>Whats happening</h2>
+      </div>
+
+      <div className="widget-container">
+        <h2>Who to follow</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Widgets;
